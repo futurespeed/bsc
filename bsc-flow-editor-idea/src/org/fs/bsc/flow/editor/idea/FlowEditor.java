4a6d49704bab5b343afa72f9c70f47281085b542
gaojingxin <gaojingxin@example.com>
2019-07-30 15:41:37 +0800

    editor

---
+++ bsc-flow-editor-idea/src/org/fs/bsc/flow/editor/idea/FlowEditor.java
@@ -0,0 +1,15 @@
+package org.fs.bsc.flow.editor.idea;
+
+import javax.swing.*;
+
+public class FlowEditor {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
